bf71981cf997863bc38e9baef93885db73fb605c
Raphael Rocha <rapharocha89@vidrelo.onmicrosoft.com>
2022-10-07 21:53:22 -0300

    Style: Remove footer from main class on home and adjust the css for mobile

--- projeto-vida/src/components/LandPage/Footer/Footer.tsx
+++ projeto-vida/src/components/LandPage/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import '../../LandPage/Footer/Style/Footer.scss'
 export default function Footer() {
   return (
     <footer id="footer">
-      <img src={Logo} alt="Logo" id='logo' />
-      <p>&copy;All Rights Reserved</p>
+      <img src={Logo}  alt="Logo" id='logo' />
+      <p><span style={{marginRight:'1rem'}}>&copy;</span>  All Rights Reserved</p>
     </footer>
   )
 }
